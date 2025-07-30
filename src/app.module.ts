6b4123321ea469ec0b3542362535f548a1cdd02a
jhoaking <peredoroj@gmail.com>
2025-07-30 15:02:22 -0400

    llevando todo a al raiz

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,11 @@ import { SeedModule } from './seed/seed.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.DATABASE_URL, 
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
     }),
